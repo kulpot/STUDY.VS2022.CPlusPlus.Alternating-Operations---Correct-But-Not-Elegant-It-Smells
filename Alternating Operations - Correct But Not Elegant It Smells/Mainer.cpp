afaec6a576c1893433404ef81bf90ee3f0c0eedb
Kulpot <kulpotwahu@gmail.com>
2023-11-03 22:59:56 +0800

    Add project files.

---
+++ Alternating Operations - Correct But Not Elegant It Smells/Mainer.cpp
@@ -0,0 +1,20 @@
+
+//ref link:https://www.youtube.com/watch?v=0eJFOJ0k0rM&list=PLRwVmtr-pp05c1HTBj1no6Fl6C6mlxYDG&index=36
+//
+
+// MyAssembly.asm - (customBuildTool)commandline - ml /c /Cx /coff "%(FullPath)"
+// MyAssembly.asm - (customBuildTool)output - %(Filename).obj
+
+// multiply overflow 1111 * 1111 = 11101001
+// 32bit overflow 1111 1111 1111 1111 1111 1111 1111 1111 * 1111 1111 1111 1111 1111 1111 1111 1111 = overflow
+
+extern "C" void doit();
+
+void main()
+{
+	doit();
+}
+
+void doit()
+{
+}
